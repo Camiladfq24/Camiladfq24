19c5ad99b7611a65b80cd32aa04eb0e019596a6f
Camiladfq24 <queirozcamila908@gmail.com>
2024-10-05 00:21:40 +0000

    fiz nada

---
+++ cdfq4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+ int x = 0;
+ printf("valor de x: %d\n", x);
+ x++;
+ printf("valor de x: &\n", x);
+ float y = 12.345678;
+ printf("valor de y: %f\n", y);
+ printf("valor de y: %.2f\n" ,y);
+ char sexo = 'M';
+ printf("sexo: %c\n",sexo);
+ char nome [30] = "joao";
+ printf("Nome: %s\n",nome);
+ printf("primeira letra: %c\n", nome[0]);
+ ("ola mundo!");
+}
